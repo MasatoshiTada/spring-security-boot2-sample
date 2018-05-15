91312b7d97a2dbfb157ca783eb553c6b44ddfea7
Masatoshi Tada <tada@casareal.co.jp>
2018-05-15 11:26:09 +0900

    first commit

---
+++ src/main/java/com/example/SpringSecurityBoot2SampleApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBoot2SampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityBoot2SampleApplication.class, args);
+    }
+}
